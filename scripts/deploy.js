fc37fd97c48b28bf0e7efbe50ffa3fd0f9353bcc
Edgar Lopez <edgarlopez241@gmail.com>
2023-04-03 10:51:37 -0400

    probando el adn

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const deploy = async () =>{
+    const [deployers]=await ethers.getSigners();
+    console.log("Deploying contract with", deployers.address)
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+    const PlatziPunk = await ethers.getContractFactory("PlatziPunk");
+    const deployed = await PlatziPunk.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    console.log("Platzi Punks was deployed at: ",deployed.address);
+};
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy().then(()=>process.exit(0)).catch(error=>{
+    console.log(error);
+    process.exit(1);
+})
